908b5d8f1d82a598ad100ed9f4974858a543488b
Zarshedjon Nasimov <2110058@newuu.uz>
2024-09-27 23:45:21 +0500

    Added project files.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5.0f;//control the speed
+    private Rigidbody2D rb;
+    public bool isDead = false;
+
+    public AudioClip jumpSound;
+    public AudioClip gameOverSound;
+    // public AudioClip scoreSound;
+    void Start()
+    {
+        //get rigibody2d Component of the BirdbBek
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GetMouseButtonDown(0) --> left side mouse button pressed down
+        if(Input.GetMouseButtonDown(0) && isDead==false){
+            rb.velocity = Vector2.up * speed;
+            SoundManager.instance.PlaySound (jumpSound);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other){
+        isDead = true;
+        SoundManager.instance.PlaySound (gameOverSound);
+    }
+}
